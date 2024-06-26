116a9c7ec627f99a9a7831744e0d11933057721a
Omar Khan <ok2610@gmail.com>
2024-06-26 17:24:04 +0000

    Update DataLoader to load one vector at a time instead of slab. Loading slab into 1D array may be preferable, but should be another function.

--- src/eCP/IDataLoader.hpp
+++ src/eCP/IDataLoader.hpp
@@ -1,89 +1,130 @@
+#ifndef I_DATA_LOADER_HPP
+#define I_DATA_LOADER_HPP
+
 #include <string>
 #include <vector>
 #include <iostream>
-#include "H5Cpp.h"
+#include <memory>
+#include <hdf5/serial/H5Cpp.h>
 
 namespace ecp {
 
     using std::vector;
     using std::string;
+    using std::unique_ptr;
 
     class IDataLoader {
-        virtual vector<vector<float>> load_data(const string& filename) = 0;
-        virtual vector<vector<float>> load_data_batch(const string& filename, int start, int end) = 0;
+        virtual ~IDataLoader() {} // Virtual desctructor
+        virtual vector<unique_ptr<float[]>> load_data(const string& filename) = 0;
+        virtual vector<unique_ptr<float[]>> load_data_batch(const string& filename, int start, int end) = 0;
+        int get_dims() {
+            return dims;
+        }
+    private:
+        int dims = 0;    
     }; // class DataLoader
 
 
     class HDF5Loader : IDataLoader {
     public:
-        HDF5Loader(string& ds_name) {
-            _ds_name = ds_name;
-        }
+        HDF5Loader(const string& ds_name) : _ds_name(ds_name) {}
 
-        ~HDF5Loader() {};
+        ~HDF5Loader() = default;
 
-        vector<vector<float>> load_data(const string& filename) override {
-            vector<vector<float>> data;
+        vector<unique_ptr<float[]>> load_data(const string& filename) override {
+            vector<unique_ptr<float[]>> data;
+
             H5::H5File file(filename, H5F_ACC_RDONLY);
             H5::DataSet dataset = file.openDataSet(_ds_name);
-            
             H5::DataSpace dataspace = dataset.getSpace();
-            hsize_t dims_out[2]; //dims_out[0] Total Vectors, dims_out[1] Vector Size
+            hsize_t dims_out[2]; // dims_out[0] Total Vectors, dims_out[1] Vector Size
+
             int ndims = dataspace.getSimpleExtentDims(dims_out, NULL);
             if (ndims != 2) {
-                std::cerr << "Invalid dataset shape!" << std::endl;
-                return data;
+                dataspace.close();
+                dataset.close();
+                file.close();
+                throw std::invalid_argument("Invalid dataset shape!");
+            }
+            dims = dims_out[1];
+
+            // Set offset and count for dataset
+            hsize_t dataOffset[2] = {0, 0};
+            const hsize_t dataCount[2] = {1, dims_out[1]};
+
+            // Define memory space for single vector
+            H5::DataSpace memspace(1, dims_out[1]);
+            const hsize_t memCount[1] = {dims_out[1]};
+            const hsize_t memOffset[1] = {0}; 
+            memspace.selectHyperslab(H5S_SELECT_SET, memCount, memOffset);
+
+            // Resize data to hold point vectors
+            data.resize(dims_out[0]);
+
+            // Read point vectors into data
+            for (hsize_t i = 0; i < dims_out[0]; ++i) {
+                dataOffset[0] = i;
+                dataspace.selectHyperslab(H5S_SELECT_SET, dataCount, dataOffset);
+                data[i] = std::make_unique<float[]>(dims_out[1]);
+                dataset.read(data[i].get(), H5::PredType::NATIVE_FLOAT, memspace, dataspace);
             }
 
-            // Read the full dataset into a buffer
-            std::vector<float> buffer(dims_out[0] * dims_out[1]);
-            dataset.read(buffer.data(), H5::PredType::NATIVE_FLOAT);
-
-            // Convert the flat buffer into a vector of vectors
-            data.reserve(dims_out[0]);
-            for (size_t i = 0; i < dims_out[0]; ++i) {
-                data.emplace_back(buffer.begin() + i * dims_out[1], buffer.begin() + (i + 1) * dims_out[1]);
-            }
-            
+            memspace.close();
             dataspace.close();
             dataset.close();
             file.close();
-            return data;
+
+            return std::move(data);
         } // HDF5Loader::load_data
 
-        vector<std::vector<float>> load_data_batch(const std::string& filename, int start, int end) override {
-            vector<std::vector<float>> data;
+        vector<unique_ptr<float[]>> load_data_batch(const std::string& filename, int start, int end) override {
+            vector<unique_ptr<float[]>> data;
 
             H5::H5File file(filename, H5F_ACC_RDONLY);
             H5::DataSet dataset = file.openDataSet(_ds_name);
-            
             H5::DataSpace dataspace = dataset.getSpace();
             hsize_t dims_out[2]; //dims_out[0] Total Vectors, dims_out[1] Vector Size
+
             int ndims = dataspace.getSimpleExtentDims(dims_out, NULL);
             if (ndims != 2) {
-                std::cerr << "Invalid dataset shape!" << std::endl;
-                return data;
+                dataspace.close();
+                dataset.close();
+                file.close();
+                throw std::invalid_argument("Invalid dataset shape!");
             }
+            dims = dims_out[1];
 
             // Check start and end are within dataset bounds
             if (start < 0 || end >= dims_out[0] || start > end) {
-                std::cerr << "Invalid start and end indices." << std::endl;
-                return data;
+                dataspace.close();
+                dataset.close();
+                file.close();
+                throw std::invalid_argument("Invalid dataset shape!");
             }
 
+            // Set range, offset, and count for the dataset
             hsize_t range = end - start + 1;
-            hsize_t count[2] = {static_cast<hsize_t>(range), dims_out[1]};
-            hsize_t offset[2] = {static_cast<hsize_t>(start), 0};
-            dataspace.selectHyperslab(H5S_SELECT_SET, count, offset);
+            const hsize_t dataOffset[2] = {static_cast<hsize_t>(start), 0};
+            const hsize_t dataCount[2] = {1, dims_out[1]};
 
-            std::vector<float> buffer(range * dims_out[1]);
-            dataset.read(buffer.data(), H5::PredType::NATIVE_FLOAT, dataspace);
+            // Define memory space for single vector
+            H5::DataSpace memspace(1, dims_out[1]);
+            const hsize_t memCount[1] = {dims_out[1]};
+            const hsize_t memOffset[1] = {0}; 
+            memspace.selectHyperslab(H5S_SELECT_SET, memCount, memOffset);
+ 
+            // Resize data to hold all point vectors
+            data.resize(range);
 
-            data.reserve(range);
+            // Read point vectors into data
             for (size_t i = 0; i < range; ++i) {
-                data.emplace_back(buffer.begin() + i * dims_out[1], buffer.begin() + (i + 1) * dims_out[1]);
+                dataOffset[0] = i;
+                dataspace.selectHyperslab(H5S_SELECT_SET, dataCount, dataOffset);
+                data[i] = std::make_unique<float[]>(dims_out[1]);
+                dataset.read(data[i].get(), H5::PredType::NATIVE_FLOAT, memspace, dataspace);
             }
 
+            memspace.close();
             dataspace.close();
             dataset.close();
             file.close();
@@ -94,3 +135,5 @@ namespace ecp {
         string _ds_name;
     }; // class HDF5Loader
 } // namespace eCP
+
+#endif // I_DATA_LOADER_HPP
